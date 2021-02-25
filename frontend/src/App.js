c036a8052e61a2bf0b6df7ff94b436ebd52d9ccf
jaaska-dev <jaaska.dev@gmail.com>
2021-02-26 01:04:06 +0200

    Installed React-Bootstrap, added a header with a navbar and a footer(copyright footnote)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,18 @@
 import { Fragment } from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
     <Fragment>
-      <h1>Welcome to Carscape</h1>
+      <Header />
+      <main className='py-4'>
+        <Container>
+          <h1>Welcome to Wall Street</h1>
+        </Container>
+      </main>
+      <Footer />
     </Fragment>
   );
 };
