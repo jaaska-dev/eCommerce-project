9276b5518a35034024b01a97b7bc64c766612c58
jaaska-dev <jaaska.dev@gmail.com>
2021-02-26 14:42:02 +0200

    Added React Router and React Router Bootstrap for links so that they won't load on a SPA

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,24 @@
-import { Fragment } from 'react';
 import { Container } from 'react-bootstrap';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 //INDIVIDUAL UI-COMPONENT IMPORTS
 import Header from './components/Header';
 import Footer from './components/Footer';
 //SCREEN IMPORTS
 import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
 
 const App = () => {
   return (
-    <Fragment>
+    <Router>
       <Header />
       <main className='py-4'>
         <Container>
-          <HomeScreen />
+          <Route path='/' component={HomeScreen} exact />
+          <Route path='/product/:id' component={ProductScreen} />
         </Container>
       </main>
       <Footer />
-    </Fragment>
+    </Router>
   );
 };
 
