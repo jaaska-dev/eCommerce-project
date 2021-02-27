db9569a405eba6b497291aff620e262cfe5fc6ab
jaaska-dev <jaaska.dev@gmail.com>
2021-02-28 00:11:55 +0200

    Added and customized the product details screen

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -3,7 +3,7 @@ import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Rating from './Rating';
 
-export const Product = ({ product }) => {
+const Product = ({ product }) => {
   return (
     <Card className='my-3 p-3 rounded'>
       <Link to={`/product/${product._id}`}>
